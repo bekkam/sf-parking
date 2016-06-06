0e5de9a6f769291e847d79896a11fef7d87338bc
U-Lenovo-PC\rmurphy <bekkam@users.noreply.github.com>
2016-06-05 23:09:42 -0700

    Add js and html for heatmap

---
+++ static/js/heatmap.js
@@ -0,0 +1,49 @@
+$( document ).ready(function() {
+    L.mapbox.accessToken = 'pk.eyJ1IjoiYmVra2EiLCJhIjoiY2lvdGllNmRuMDBicXVjbThjeWVjZnNxayJ9.HlEvEtM4nNTSonVEtFtaCw';
+    var map = L.mapbox.map('map', 'mapbox.streets', {
+            zoomControl: true
+        }).setView([37.780, -122.419], 14);
+
+
+        // HEATMAP
+        $.getJSON('/getheatmap').done(function(data){ 
+            console.log(data);
+            heat = L.heatLayer([], {
+                radius: 10,
+                blur: 7, 
+                maxZoom: 16
+            });
+            feature_layer = L.mapbox.featureLayer(data);   
+            heat.addTo(map);                            
+            feature_layer.on('click', function(e) {     
+            });
+            feature_layer.eachLayer(function(l) {       
+                heat.addLatLng(l.getLatLng());
+            });        
+        });
+
+    // ADD DIRECTIONS PANEL
+    // move the attribution control out of the way
+    map.attributionControl.setPosition('bottomleft');
+
+    // create the initial directions object, from which the layer
+    // and inputs will pull data.
+    var directions = L.mapbox.directions({
+        profile: 'mapbox.cycling'
+    });
+
+    var directionsLayer = L.mapbox.directions.layer(directions)
+        .addTo(map);
+
+    var directionsInputControl = L.mapbox.directions.inputControl('inputs', directions)
+        .addTo(map);
+
+    var directionsErrorsControl = L.mapbox.directions.errorsControl('errors', directions)
+        .addTo(map);
+
+    var directionsRoutesControl = L.mapbox.directions.routesControl('routes', directions)
+        .addTo(map);
+
+    var directionsInstructionsControl = L.mapbox.directions.instructionsControl('instructions', directions)
+        .addTo(map);
+});
